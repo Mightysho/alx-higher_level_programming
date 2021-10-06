bdc3a8092ee843a11f9d5200ea20182b78b1a311
Micheal Shokunbi <mightysho4sure2014@gmail.com>
2021-10-06 04:37:24 +0100

    0x07-python-test_driven_development

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,30 +1,23 @@
 #!/usr/bin/python3
-'''this module do stuff to strings'''
+"""
+This is the text_indentation module.
+This module supplies one function, text_indentation().
+"""
 
 
 def text_indentation(text):
-    '''this function fixes text indentation
-    args:
-        text (str): text to fix
-    returns:
-        None
-    '''
-
+    """
+    Print a text with 2 new lines after each of these characters: ., ? and :.
+    Args:
+        text (str): the text to print.
+    """
     if type(text) is not str:
-        raise TypeError('text must be a string')
+        raise TypeError("text must be a string")
 
-    fst_idx = 0
-    for idx, char in enumerate(text):
-        if char == '.' or char == '?' or char == ':':
-            if (idx + 1) == len(text):
-                tmp_str = text[fst_idx:]
-            else:
-                tmp_str = text[fst_idx:idx + 1]
-            fst_idx = idx + 1
-            print(tmp_str.strip())
-            print()
-            continue
-
-        if idx == len(text) - 1:
-            tmp_str = text[fst_idx:]
-            print(tmp_str.strip())
+    line = ""
+    for c in range(len(text)):
+        line += text[c]
+        if text[c] in ".?:":
+            print((line + '\n').lstrip(' '))
+            line = ""
+    print(line.lstrip(' '), end='')
