1968e7343a8aa599700845e34c8cf2d6b08d35bb
Micheal Shokunbi <mightysho4sure2014@gmail.com>
2021-10-06 04:34:52 +0100

    0x07-python-test_driven_development

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-'''This module for printing stuff'''
+"""
+This is the say_my_name module.
+
+This module supplies one function, say_my_name().
+"""
 
 
 def say_my_name(first_name, last_name=""):
-    '''This function print the string "My name is <first name> <last name>"
-    args:
-        first_name (str): first name
-        last_name (str): last name
-    returns:
-        None
-    '''
+    """
+    Print My name is <first name> <last name>.
 
+    Args:
+        first_name (str): the first name.
+        last_name (str): the last name.
+    """
     if type(first_name) is not str:
-        raise TypeError('first_name must be a string')
-    elif type(last_name) is not str:
-        raise TypeError('last_name must be a string')
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
 
-    print(f'My name is {first_name} {last_name}')
+    print("My name is {:s} {:s}".format(first_name, last_name))
