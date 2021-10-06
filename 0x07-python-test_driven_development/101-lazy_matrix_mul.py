276da715fa53a091ee6808aca422d5af7beafe55
Micheal Shokunbi <mightysho4sure2014@gmail.com>
2021-10-06 04:31:03 +0100

    0x07-python-test_driven_development

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
-'''this module for matrices calculations using numpy'''
-
-
+"""
+This is the lazy_matrix_mul module.
+This module supplies one function, lazy_matrix_mul().
+"""
 import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
-    '''multipy 2 patrices with numpy'''
+    """
+    Return a new matrix where each element has been divided by div.
+    Args:
+        m_a (list): list of lists of integers or floats.
+        m_b (list): list of lists of integers or floats.
+    """
+
     return np.dot(m_a, m_b)
